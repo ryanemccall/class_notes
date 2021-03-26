250d8f2a51112f8847e062bc284ddc0aa7162b3c
Ryan Modrall <ryanemccall20@gmail.com>
2021-03-25 21:18:33 -0400

    updating some stuff

---
+++ html_lesson/day_2_notes.js
@@ -0,0 +1,14 @@
+// var vs let vs const
+
+//Git
+//Create reference in a chronological timeline
+//upload everything so if your computer fails or crashes you have your sourcecode in the cloud
+// in terminal to go back it is cd ..
+// Staging files for a commit
+//git add <file_name> OR git add . to add all of the files inside of the folder you are in
+//git status is a command that give you the status of the branches/commits
+//git commit -m "Your commit message here" Usually a short message of just what you are doing
+//to view the commit: git log
+// Go to github.com and click create repository 
+//name it and leave it public
+// copy the url and use git remote add origin <url>
